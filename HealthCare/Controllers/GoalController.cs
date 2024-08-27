c5747685614bb0980d925df0e47a004d8c434ab5
DevinStarnes <devinstarnes98765@gmail.com>
2024-08-27 19:17:48 +0900

    Built initial structure

---
+++ HealthCare/Controllers/GoalController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCare.Controllers
+{
+    public class GoalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Summary()
+        {
+            return View();
+        }
+    }
+}
