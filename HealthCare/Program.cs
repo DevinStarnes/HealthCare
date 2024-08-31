0d72ef43613d3d6d060b8c36b4510199ecc5de81
DevinStarnes <devinstarnes98765@gmail.com>
2024-09-01 02:11:01 +0900

    complete login logic

--- HealthCare/Program.cs
+++ HealthCare/Program.cs
@@ -11,10 +11,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<HealthCareUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
